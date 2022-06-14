c4c2721e6f22ad39c82e792a50abf7927dbf2180
NatashaBazunova <natashaiosifova@mail.ru>
2022-06-14 17:03:55 +0500

    Second task added

--- src/test/java/ru/netology/web/OrderTest.java
+++ src/test/java/ru/netology/web/OrderTest.java
@@ -3,6 +3,7 @@ package ru.netology.web;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -49,6 +50,77 @@ public class OrderTest {
         $("button").click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+    @Test
+    public void shouldNotSubmitEnglishName() {
+        $("[data-test-id=name] input").setValue("Ivanov Ian");
+        $("[data-test-id=phone] input").setValue("+30666345792");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    public void shouldNotSubmitWrongPhone() {
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("80666345792");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $(".input_type_tel .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    public void shouldNotSubmitWithoutAgreement() {
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+70666345792");
+//        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $(".checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
+
+    @Test
+    public void shouldSubmitWithLongName() {
+        $("[data-test-id=name] input").setValue("Ивановаыпаывплыопмиукшгпгщшиппмлывоирфпжшпоимиыуыпшгмыитвлаопгш Иванцволапсадлоывамишапмишпмшпиглпшдгфрымшпашгйн");
+        $("[data-test-id=phone] input").setValue("+70666345792");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    public void shouldNotSubmitShortPhone() {
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+706663492");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $(".input_type_tel .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+    @Test
+    public void shouldSubmitWithThreeNames() {
+        $("[data-test-id=name] input").setValue("Иванов Иван Иванович");
+        $("[data-test-id=phone] input").setValue("+70666345792");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    public void shouldSubmitWithLetter() {
+        $("[data-test-id=name] input").setValue("Ивайнова Алина");
+        $("[data-test-id=phone] input").setValue("+70666345792");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Disabled
+    @Test
+    public void shouldNotSubmitWithLetter() {
+        $("[data-test-id=name] input").setValue("Иванова Алёна");
+        $("[data-test-id=phone] input").setValue("+70666345792");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+
 }
 
 
